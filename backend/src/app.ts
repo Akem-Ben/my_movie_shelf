a393039e36c40c0e0e15cb0ab3c4faacf48e08af
Akem-Ben <akemini.ndaobong@gmail.com>
2024-12-03 11:54:31 +0100

    server setup

---
+++ backend/src/app.ts
@@ -0,0 +1,63 @@
+import express, { NextFunction, Request, Response } from "express";
+import helmet from "helmet";
+import compression from "compression";
+import cors from "cors";
+import bodyParser from "body-parser";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+// import apiRouter from "./routes";
+import dotenv from 'dotenv';
+import { createServer } from "http";
+
+const app = express();
+
+const server = createServer(app);
+
+dotenv.config()
+
+// Set security HTTP headers to disable 'powered by Express' header feature
+app.disable("x-powered-by");
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// Compress response to increase speed
+app.use(compression());
+
+// Set Cors
+app.use(cors());
+
+//Other Middlewares
+app.use(bodyParser.json());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(cookieParser());
+
+// Database
+
+// Routes
+// app.use("/api", apiRouter);
+
+
+// Health Check Endpoint
+app.get("/", (request: Request, response: Response) => {
+  response.send("Welcome to Deep Tech Industries' Backend Server. 👋");
+});
+
+
+// Error handler
+// app.use(errorUtilities.globalErrorHandler);
+
+const PORT = 3000
+
+/**
+ * Server
+ */
+server.listen(PORT, () => {
+  console.log(`server running on Port ${PORT}`);
+});
+
+export default app;
